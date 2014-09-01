289b42ee16e08754a2b5555302b2135517c6cfc4
dtaniwaki <daisuketaniwaki@gmail.com>
2014-09-02 02:47:56 +0900

    Refactoring core logic

--- lib/faraday_middleware/retry_proxy_0_8.rb
+++ lib/faraday_middleware/retry_proxy_0_8.rb
@@ -1,35 +1,10 @@
-module FaradayMiddleware
-  class RetryProxy < Faraday::Middleware
-    TARGET_ERRORS = [Errno::ETIMEDOUT, Timeout::Error, Faraday::Error::TimeoutError, Faraday::Error::ConnectionFailed]
+require_relative 'retry_proxy_core'
 
+module FaradayMiddleware
+  class RetryProxy < RetryProxyCore
     def initialize(app, options = {})
       super(app)
       @options = options
-    end
-
-    def call(env)
-      env[:request] ||= {}
-      proxies = self.normalize_proxies(env[:request][:proxy])
-      request_body = env[:body]
-      begin
-        proxy = proxies.pop
-        env[:request][:proxy] = proxy ? {uri: URI.parse(proxy)} : nil
-        env[:body] = request_body
-        @app.call(env)
-      rescue *TARGET_ERRORS => exception
-        if proxies.size > 0
-          sleep (1 + SecureRandom.random_number * @options[:interval_randomness].to_f) * @options[:interval].to_f
-          retry
-        end
-        raise
-      end
-    end
-
-    def normalize_proxies(proxy)
-      if proxy.respond_to?(:call)
-        proxy = proxy.call(self)
-      end
-      proxy ? Array(proxy) : []
     end
   end
 end
--- lib/faraday_middleware/retry_proxy_0_9.rb
+++ lib/faraday_middleware/retry_proxy_0_9.rb
@@ -1,7 +1,7 @@
-module FaradayMiddleware
-  class RetryProxy < Faraday::Middleware
-    TARGET_ERRORS = [Errno::ETIMEDOUT, Timeout::Error, Faraday::Error::TimeoutError, Faraday::Error::ConnectionFailed]
+require_relative 'retry_proxy_core'
 
+module FaradayMiddleware
+  class RetryProxy < RetryProxyCore
     class Options < Faraday::Options.new(:interval, :interval_randomness)
       def self.from(value)
         if Fixnum === value
@@ -23,31 +23,6 @@ module FaradayMiddleware
     def initialize(app, options = nil)
       super(app)
       @options = Options.from(options)
-    end
-
-    def call(env)
-      env[:request] ||= {}
-      proxies = self.normalize_proxies(env[:request][:proxy])
-      request_body = env[:body]
-      begin
-        proxy = proxies.pop
-        env[:request][:proxy] = proxy ? {uri: URI.parse(proxy)} : nil
-        env[:body] = request_body
-        @app.call(env)
-      rescue *TARGET_ERRORS => exception
-        if proxies.size > 0
-          sleep (1 + SecureRandom.random_number * @options[:interval_randomness].to_f) * @options[:interval].to_f
-          retry
-        end
-        raise
-      end
-    end
-
-    def normalize_proxies(proxy)
-      if proxy.respond_to?(:call)
-        proxy = proxy.call(self)
-      end
-      proxy ? Array(proxy) : []
     end
   end
 end
